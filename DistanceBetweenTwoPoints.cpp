932267db72e252ae460b2d24072068cd1896e8e8
Wonbeen Kim <31658667+Wonbeen@users.noreply.github.com>
2023-03-04 18:07:48 -0500

    Update DistanceBetweenTwoPoints.cpp

--- DistanceBetweenTwoPoints.cpp
+++ DistanceBetweenTwoPoints.cpp
@@ -1,2 +1,12 @@
+/*
+Author: Ian Wonbeen Kim
+Date: Feb. 25, 2023
+Language: Houdini Vex
+Program Version: Houdini FX Education Version 19.5.303
+Category: Vex, Sop, Point Wrangle, Details
+Key Functions: addpoint(geoself(), vector3);
+Code Function: Create Points with x,y,z positions based on the nested for-loop iterations.
+*/
+
 float d = length(@P); //length from the origin to the point P
 float dist = distance(@P, {2,0,5}); //length from one point to another. Here the distance from @P and point with coordinates defined by {2,0,5}.
