b21ef1a1b50adca1865240f17f5ca72fdca105cf
Wonbeen Kim <31658667+Wonbeen@users.noreply.github.com>
2023-03-04 18:08:46 -0500

    Update DistanceBetweenTwoPoints.cpp

--- DistanceBetweenTwoPoints.cpp
+++ DistanceBetweenTwoPoints.cpp
@@ -8,5 +8,5 @@ Key Functions: addpoint(geoself(), vector3);
 Code Function: DistanceBetween2Points
 */
 
-float d = length(@P); //length from the origin to the point P
+float len = length(@P); //length from the origin to the point P
 float dist = distance(@P, {2,0,5}); //length from one point to another. Here the distance from @P and point with coordinates defined by {2,0,5}.
